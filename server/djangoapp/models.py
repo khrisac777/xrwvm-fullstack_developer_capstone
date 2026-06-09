b329b231eb589e582984bb21fadccd6f24b815f1
khrisac777 <christian.carchi@unach.edu.ec>
2026-06-09 13:26:12 -0500

    Se agregan modelos CarMake y CarModel, panel de admin y script de poblacion de BD

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -23,3 +23,35 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+# Modelo para la Marca del Auto
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name  # Devuelve el nombre de la marca
+
+# Modelo para el Modelo del Auto
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Relación de Muchos a Uno
+    dealer_id = models.IntegerField()  # ID que se referirá al concesionario en Mongo
+    name = models.CharField(max_length=100)
+    
+    # Opciones limitadas para el tipo de auto
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    
+    # Validador de años
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+
+    def __str__(self):
+        # Devuelve la marca y el modelo juntos (Ej. "Toyota Corolla")
+        return f"{self.car_make.name} {self.name}"
