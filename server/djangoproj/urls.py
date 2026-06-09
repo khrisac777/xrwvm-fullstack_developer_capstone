e8cfbf03e547072ddd512be3f8fc27611644ad4e
khrisac777 <christian.carchi@unach.edu.ec>
2026-06-09 15:45:09 -0500

    Fix: PEP-8 formatting and Flake8 linting errors.3

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -31,9 +31,11 @@ urlpatterns = [
         'dealers/', TemplateView.as_view(template_name="index.html")
         ),
     path(
-        'dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")
+        'dealer/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
         ),
     path(
-        'postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")
+        'postreview/<int:dealer_id>',
+        TemplateView.as_view(template_name="index.html")
         ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
